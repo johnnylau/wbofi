0811da5defd9bb61719d6d1ee01b7e5abc6eb9e1
Lantares <185191430@qq.com>
2014-10-17 01:31:02 +0800

    增加了命令行的选项
    -h， --help 帮助
    -v， --version 版本
    -p, --path 路径（不指定为～）
    -u, --user 用户
    --headicon 获取头像
    示例运行如下
    
    python weibo_spider.py -u hanhan --headicon

--- weibo_spider.py
+++ weibo_spider.py
@@ -1,5 +1,9 @@
 #-*- coding: utf-8 -*-
 import re, requests, signal, sys, shutil
+import sys
+import getopt
+
+
 from lxml import etree
 from os.path import expanduser
 
@@ -34,8 +38,11 @@ def getOid(pageContent):
 		print "没有找到这个用户"
 		sys.exit(1)
 
-def getHeadPic(userName, pageContent):
-	path = expanduser("~")+'/'+userName+'.jpg'
+def getHeadPic(userName, pageContent, path):
+	if path=='':
+		path = expanduser("~")+'/'+userName+'.jpg'
+	else:
+		path = path+'/'+userName+'.jpg'
 	xhtml = etree.HTML(pageContent.text)
 	imgLis = xhtml.xpath('//div[@class="pf_head_pic"]/img/@src')
 	loadedImg = requests.get(imgLis[0], stream=True)
@@ -48,11 +55,63 @@ def getHeadPic(userName, pageContent):
 def getFerList(userName, pageContent):
 	pass
 
-if __name__ == "__main__":
+
+def getHeadIcon(path, userId):
 	cheTimeOut(handleTimeOut, 8)
-	userId = 'hanhan'
 	content = getContent(userId)
 	userName = getName(content)
 	userOid = getOid(content)
-	getHeadPic(userName, content)
+	getHeadPic(userName, content, path)
 	print "用户名: "+userName
+
+def usage():
+    print "wbofi usage:"
+    print "-h --help: help message"
+    print "-v --version: print version"
+    print "-p --path: set output path"
+    print "-u --user: set user id or name"
+    print "--headicon: get user headIcon"
+
+def version():
+    print "wbofi version 0.0.0.0"
+
+
+def main(argv):
+    path = ''
+    user = ''
+    try :
+        opts, args = getopt.getopt(argv[1:], 'hvp:u:',['help', 'version', 'path=', 'user=', 'headicon'])
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(2)
+
+    for o, a in opts:
+        if o in('-h', '--help'):
+            usage()
+            sys.exit(1)
+        elif o in ('-v', '--version'):
+            version()
+            sys.exit(1)
+        elif o in ('-p', '--path'):
+            path = a
+        elif o in ('-u', '--user'):
+            user = a
+        elif o in ('--headicon'):
+            getHeadIcon(path, user)
+            sys.exit(1)
+        else:
+            sys.exit(3)
+
+if __name__ == "__main__":
+	main(sys.argv)
+
+
+
+
+
+
+
+
+
+
